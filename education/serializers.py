bb511a7cd5ed7787f35f4435c6e0e729d7ffdfd6
Helen Chernotalova <chernotalova.e@yandex.ru>
2025-04-28 15:45:29 +0400

    Расширение сериализатора модели курса

--- education/serializers.py
+++ education/serializers.py
@@ -1,9 +1,15 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from education.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+
+    def get_lesson_count(self, obj):
+        return obj.lesson_set.all().count()
+
     class Meta:
         model = Course
         fields = "__all__"
