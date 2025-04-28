ae753ea2a434c4df6c30b57c99dcb7d0bdf583f7
Helen Chernotalova <chernotalova.e@yandex.ru>
2025-04-28 16:31:39 +0400

    Модель платежей ит кастомная команда

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from education.models import Course
+
 
 class User(AbstractUser):
     username = None
@@ -23,3 +25,23 @@ class User(AbstractUser):
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
         ordering = ["email"]
+
+
+class Payment(models.Model):
+    CASH = "cash"
+    ONLINE_PAYMENT = "online_payment"
+
+    STATUS_PAYMENT = [
+        (CASH, "Наличные"),
+        (ONLINE_PAYMENT, "Перевод на счет"),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, verbose_name="Пользователь")
+    date_pay = models.DateTimeField(verbose_name="Дата и время оплаты")
+    content_pay = models.ForeignKey(Course, on_delete = models.CASCADE)
+    payment_amount = models.IntegerField(verbose_name="Сумма оплаты")
+    payment_method = models.CharField(
+        max_length=15,
+        choices=STATUS_PAYMENT,
+        default=ONLINE_PAYMENT,
+        verbose_name="Способ оплаты",
+    )
