c83bd4e69d88621b9a4673d9dde0493eb61b822c
Helen Chernotalova <chernotalova.e@yandex.ru>
2025-05-11 17:39:14 +0500

    Реализация аутентификации через JWT, настройка доступа к эндпоинтам

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import PaymentListView
+from users.views import PaymentListView, UserCreateAPIView
 
 app_name = UsersConfig.name
 
 
 urlpatterns = [
     path("users_list/", PaymentListView.as_view(), name="user_list"),
+    path("login/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='token_obtain_pair'),
+    path("token/refresh/", TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
+    path("register/", UserCreateAPIView.as_view(), name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.permissions import AllowAny
 
-from .models import Payment
-from .serializers import PaymentSerializer
+from .models import Payment, User
+from .serializers import PaymentSerializer, UserSerializer
 
 
 class PaymentListView(ListAPIView):
@@ -14,3 +15,14 @@ class PaymentListView(ListAPIView):
         "date_pay",
     ]
     filterset_fields = ("content_pay", "payment_method")
+
+
+class UserCreateAPIView(CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
