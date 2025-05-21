ec864ebbe84c583b3bcf0aadf11c46d9bac45f62
Helen Chernotalova <chernotalova.e@yandex.ru>
2025-05-21 14:44:15 +0500

    Настройка проекта для работы с Celery и Radis

--- config/settings.py
+++ config/settings.py
@@ -143,3 +143,13 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=45),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
 }
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+
+CELERY_TIMEZONE = "Europe/Moscow"
+
+CELERY_TASK_TRACK_STARTED = True
+
+CELERY_TASK_TIME_LIMIT = 30 * 60
